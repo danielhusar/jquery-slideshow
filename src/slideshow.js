fd076fabf319d57a3a9251cda75333ec8b53b946
Daniel Husar <dano.husar@gmail.com>
2015-06-30 15:14:44 +0100

    init files

---
+++ src/slideshow.js
@@ -0,0 +1,56 @@
+(function (window, document, undefined) {
+  'use strict';
+
+  window.slideshow = function (wrap, current, opts) {
+    opts = opts || {};
+
+    var $wrap = document.querySelector(wrap);
+    var $items = $wrap.querySelectorAll(opts.itemClass || '.item');
+    var $prev = $wrap.querySelectorAll(opts.itemClass || '.prev');
+    var $next = $wrap.querySelectorAll(opts.itemClass || '.next');
+    var hidden = opts.hiddenClass || 'hidden';
+    var max = $items.length;
+    current = current === undefined ? 1 : current;
+
+    forEach($prev, function (el) {
+      el.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (current - 1 > 0) {
+          goTo(current - 1);
+        }
+      });
+    });
+
+    forEach($next, function (el) {
+      el.addEventListener('click', function (e) {
+        e.preventDefault();
+        if (current + 1 <= max) {
+          goTo(current + 1);
+        }
+      });
+    });
+
+    function goTo (slide) {
+      current = slide;
+
+      forEach($items, function (el, index) {
+        if (++index !== slide) {
+          el.classList.add(hidden);
+        } else {
+          el.classList.remove(hidden);
+        }
+      });
+
+      $wrap.classList.toggle('first-item', current === 1);
+      $wrap.classList.toggle('last-item', current === max);
+    }
+
+    function forEach (selector, cb) {
+      return [].forEach.call(selector, cb);
+    }
+
+    goTo(current);
+    return $wrap;
+  };
+
+}(this, this.document));
--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,69 @@
 var assert = chai.assert;
 
-describe('jquery-slideshow', function () {
+describe('Slideshow', function () {
 
-  it('Should work', function (cb) {
-    assert(true);
+  var $slideshow = window.slideshow('.slideshow');
+
+  it('Init should work', function (cb) {
+    assert(!$slideshow.querySelector('.item-1').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-2').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-3').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-4').classList.contains('hidden'));
+    assert($slideshow.classList.contains('first-item'));
+    assert(!$slideshow.classList.contains('last-item'));
     cb();
+  });
+
+  it('Navigation forward works', function () {
+    var $next = $slideshow.querySelector('.next');
+
+    $next.click();
+
+    assert($slideshow.querySelector('.item-1').classList.contains('hidden'));
+    assert(!$slideshow.querySelector('.item-2').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-3').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-4').classList.contains('hidden'));
+
+    $next.click();
+    $next.click();
+
+    assert($slideshow.querySelector('.item-1').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-2').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-3').classList.contains('hidden'));
+    assert(!$slideshow.querySelector('.item-4').classList.contains('hidden'));
+
+    $next.click();
+
+    assert($slideshow.querySelector('.item-1').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-2').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-3').classList.contains('hidden'));
+    assert(!$slideshow.querySelector('.item-4').classList.contains('hidden'));
+  });
+
+  it('Navigation backwards works', function () {
+    var $prev = $slideshow.querySelector('.prev');
+
+    $prev.click();
+
+    assert($slideshow.querySelector('.item-1').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-2').classList.contains('hidden'));
+    assert(!$slideshow.querySelector('.item-3').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-4').classList.contains('hidden'));
+
+    $prev.click();
+    $prev.click();
+
+    assert(!$slideshow.querySelector('.item-1').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-2').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-3').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-4').classList.contains('hidden'));
+
+    $prev.click();
+
+    assert(!$slideshow.querySelector('.item-1').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-2').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-3').classList.contains('hidden'));
+    assert($slideshow.querySelector('.item-4').classList.contains('hidden'));
   });
 
 });
